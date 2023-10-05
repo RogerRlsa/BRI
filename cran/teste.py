03beb3a0918d6f249899fbbf16d7c0bea772297a
RogerRlsa <90945693+RogerRlsa@users.noreply.github.com>
2023-10-05 08:36:12 -0300

    Add files via upload

--- cran/teste.py
+++ cran/teste.py
@@ -1,5 +1,5 @@
 import timeit
-
+from multiprocessing import process
 
 def lerColecao(path):
     cont = ""
@@ -14,11 +14,39 @@ def addIndice(doc):
     #doc = doc.split()
     indice.append(doc)
 
+def extrairExpandir(texto):
+    return [texto]
+
+def comparar(q, i)-> bool:
+    palavrasQ = set(q[0].split(" "))
+    palavrasD = set(i.split(" "))
+    #print(q)
+    intdq = palavrasD.intersection(palavrasQ)
+    #print(palavrasQ)
+    #print()
+    #print(len(intdq))
+    #exit()
+    if (len(intdq)/len(palavrasQ)) > 0.35:
+        return True
+    else:
+        return False
+
+def buscar(q):
+    resultados = []
+    for i in enumerate(indice):
+        if comparar(q,i[1]):
+            resultados.append(i[0])
+    return resultados
+    
+def rankear(resultados):
+    
+    pass
 
 indice = list()
 
+#arquivo carn
 documentos = lerColecao("cran.all.1400")
-print("#docs:",len(documentos))
+print("#docs:",len(documentos),"\n")
 
 antesLixo = timeit.default_timer()
 
@@ -29,7 +57,29 @@ for doc in documentos:
 
 depoisLixo = timeit.default_timer() - antesLixo
 
-print("%f" %(depoisLixo))
+print("%f" %(depoisLixo),"\n")
 
+#arquivo qry
+qry = lerColecao("cran.qry")
+print("#qry:",len(qry),"\n")
+
+for con in qry[1:-1]:
+    #print(con)
+    consultaLimpa = removerLixo(con)
+    #print(consultaLimpa)
+    listaConsulta = extrairExpandir(consultaLimpa)
+    #print(listaConsulta)
+    listaResultados = []
+    resultados = buscar(listaConsulta) # resultados de uma pesquisa
+    
+    listaResultados.append(resultados) # resultados de todas as pesquisa
+
+    rankear(listaResultados)
+
+    print(resultados)
+    break
+
+
+#print("docs:",indice[1])
 #consultas = lerColecao("cran.qry")
 #print("#qry:",len(consultas))
